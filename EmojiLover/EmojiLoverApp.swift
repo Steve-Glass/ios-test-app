16e40e87d1371896d23f5d6b9780eed86ffc45ef
Steve-Glass <steve-glass@github.com>
2023-05-19 16:03:37 -0400

    Initial Commit

---
+++ EmojiLover/EmojiLoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiLoverApp.swift
+//  EmojiLover
+//
+//  Created by Stephen Glass on 5/19/23.
+//
+
+import SwiftUI
+
+@main
+struct EmojiLoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
